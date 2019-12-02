9e91bfa71409e24183141d366f10b2a022386c11
Jeremy Osterhouse <jeremy@onetencommunications.com>
2019-12-02 11:13:34 -0500

    Day 1, part 2

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -11,12 +11,29 @@ namespace AdventOfCode
             var input = File.ReadAllLines("day1.txt").ToList();
             var numInput = input.Select(double.Parse);
             double sum = 0;
+            double sum2 = 0;
             foreach (var line in numInput)
             {
                 sum += Math.Floor(line / 3) - 2;
             }
-            Console.WriteLine(sum);
+            Console.WriteLine("Part 1: {0}", sum);
+
+            foreach (var line in numInput)
+            {
+                sum2 += CalculateFuel(line);
+            }
+            Console.WriteLine("Part 2: {0}", sum2);
             Console.ReadKey();
+        }
+
+        static double CalculateFuel(double weight, double total = 0)
+        {
+            var result = Math.Floor(weight / 3) - 2;
+            if (result > 0)
+            {
+                return CalculateFuel(result, total + result);
+            }
+            return total;
         }
     }
 }
