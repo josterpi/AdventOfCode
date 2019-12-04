f75d421d55b1d68864be6fdee5a0501d12b52510
Jeremy Osterhouse <jeremy@onetencommunications.com>
2019-12-04 12:55:01 -0500

    Day 4, part 2

--- AdventOfCode/day04/Day4.cs
+++ AdventOfCode/day04/Day4.cs
@@ -12,6 +12,7 @@ namespace AdventOfCode.day04
             var inputMax = 843212;
 
             var passwordCandidates = new List<string>();
+            
             for (var i = inputMin; i <= inputMax; i++)
             {
                 var pwdCandidate = i.ToString();
@@ -21,23 +22,49 @@ namespace AdventOfCode.day04
                 }
             }
             Console.WriteLine("Part 1: {0}", passwordCandidates.Count);
+            var part2Count = 0;
+            foreach (var candidate in passwordCandidates)
+            {
+                if (TestPassword(candidate, part2: true)) part2Count++;
+            }
+            Console.WriteLine("Part 2: {0}", part2Count);
+            /*
+            Console.WriteLine("111111: {0}", TestPassword("111111"));
+            Console.WriteLine("223450: {0}", TestPassword("223450"));
+            Console.WriteLine("123789: {0}", TestPassword("123789"));
+            Console.WriteLine("112233: {0}", TestPassword("112233", true));
+            Console.WriteLine("123444: {0}", TestPassword("123444", true));
+            Console.WriteLine("111122: {0}", TestPassword("111122", true));
+            */
         }
 
-        static bool TestPassword(string pwd)
+        static bool TestPassword(string pwd, bool part2 = false)
         {
             // password length must be 6
             if (pwd.Length != 6) return false;
             // two adjacent digits are the same
-            var sameAdjecent = false;
+            var adjacentLengths = new List<int>();
+            var lengthAdjacent = 1;
             for (var i = 0; i < pwd.Length - 1; i++)
             {
                 if (pwd[i] == pwd[i+1])
                 {
-                    sameAdjecent = true;
-                    break;
+                    lengthAdjacent++;
+                } else
+                {
+                    if (lengthAdjacent > 1) adjacentLengths.Add(lengthAdjacent);
+                    lengthAdjacent = 1;
                 }
             }
-            if (!sameAdjecent) return false;
+            // For when all digits are the same
+            if (lengthAdjacent > 1) adjacentLengths.Add(lengthAdjacent);
+            if (part2)
+            {
+                if (!adjacentLengths.Contains(2)) return false;
+            } else
+            {
+                if (adjacentLengths.Count == 0) return false;
+            }
             // digits never decrease
             var neverDecrease = true;
             for (var i = 0; i < pwd.Length - 1; i++)
