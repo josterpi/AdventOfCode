a6a17fad57772202d5a414485d0be859390f8764
Jeremy Osterhouse <jeremy@onetencommunications.com>
2019-12-04 11:04:46 -0500

    Day 2, part 2
    Fixed the bug where I was using the modfied state from part 1 for part 2.

--- AdventOfCode/day02/Day2.cs
+++ AdventOfCode/day02/Day2.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Collections.Generic;
 using System.Text;
 using System.Linq;
+using System.Diagnostics;
 
 namespace AdventOfCode.day02
 {
@@ -12,30 +13,74 @@ namespace AdventOfCode.day02
         {
             var rawInput = File.ReadAllText("day02/day2.txt");
             var input = rawInput.Split(",").ToList().Select(int.Parse).ToList();
+                                  
+            Console.WriteLine("Part 1: {0}", Intcode(input, 12, 2));
 
-            // Setup 1202 program alarm
-            input[1] = 12; input[2] = 2;
+            // Which noun & verb result in 19690720?
+            var searchResult = 19690720;
+            
+            // Search combination of small number and move larger, rather than testing (0, 0-99), (1, 0-99), etc.
+            for (var box = 0; ; box++)
+            {
+                for (var noun = 0; noun <= box; noun++)
+                {
+                    //Console.WriteLine("{0} - {1}", noun, box);
+                    if (TryPartTwo(input, noun, box, searchResult)) return;
+                }
+                for (var verb = 0; verb < box; verb++)
+                {
+                    //Console.WriteLine("{0} - {1}", box, verb);
+                    if (TryPartTwo(input, box, verb, searchResult)) return;
+                }
+            }
+            Console.WriteLine("No solution 2");
+        }
+
+        static bool TryPartTwo(List<int> prog, int noun, int verb, int searchResult)
+        {
+            try
+            {
+                var result = Intcode(prog, noun, verb);
+                
+                if (result == searchResult)
+                {
+                    Console.WriteLine("Part 2: {0}", 100 * noun + verb);
+                    return true;
+                }
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+
+            }
+            return false;
+        }
+
+        static int Intcode(List<int> prog, int noun, int verb)
+        {
+            var mem = new List<int>(prog);
+            
+            mem[1] = noun; mem[2] = verb;
 
             var position = 0;
             int opcode, operand1, operand2, result;
-            while (input[position] != 99)
+            while (mem[position] != 99)
             {
-                opcode = input[position];
-                operand1 = input[position + 1];
-                operand2 = input[position + 2];
-                result = input[position + 3];
+                opcode = mem[position];
+                operand1 = mem[position + 1];
+                operand2 = mem[position + 2];
+                result = mem[position + 3];
                 switch (opcode)
                 {
                     case 1:
-                        input[result] = input[operand1] + input[operand2];
+                        mem[result] = mem[operand1] + mem[operand2];
                         break;
                     case 2:
-                        input[result] = input[operand1] * input[operand2];
+                        mem[result] = mem[operand1] * mem[operand2];
                         break;
                 }
                 position += 4;
             }
-            Console.WriteLine("Part 1: {0}", input[0]);
+            return mem[0];
         }
     }
 }
