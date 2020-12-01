05460611dd32f320a40d84454fb768522a9b779c
Jeremy Osterhouse <josterhouse@gmail.com>
2020-12-01 13:13:01 -0500

    Day 1, Part 2

--- day1.py
+++ day1.py
@@ -4,9 +4,13 @@ def main():
     for i, item1 in enumerate(input):
         for j, item2 in enumerate(input):
             if i != j:
-                if item1 + item2 == 2020:
-                    print(item1 * item2)
-                    return
+                if item1 + item2 == 2020 and not part1_solved:
+                    print("Part 1: %s\n" % (item1 * item2))
+                for k, item3 in enumerate(input):
+                    if j != k:
+                        if item1 + item2 + item3 == 2020:
+                            print("Part 2: %s\n" % (item1 * item2 * item3))
+                            return
 
 
 if __name__ == '__main__':
