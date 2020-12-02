a8c525872fc62426754a3388f9d8c953e5b4ab55
Jeremy Osterhouse <josterhouse@gmail.com>
2020-12-02 10:13:45 -0500

    Day 2, Part 2

--- day2.py
+++ day2.py
@@ -6,12 +6,17 @@ def count_letter(char, hay):
 
 def main():
     good = 0
+    good2 = 0
     for line in open("day2.txt"):
         low, high, char, pwd = re.match(r"(\d+)-(\d+) ([a-z]): ([a-z]+)", line.strip()).groups()
         count = count_letter(char, pwd)
         if int(low) <= count and count <= int(high):
             good += 1
+        # bool(a) ^ bool(b) is XOR
+        if bool(pwd[int(low)-1] == char) ^ bool(pwd[int(high)-1] == char):
+            good2 += 1
     print(f"Part 1: {good}")
+    print(f"Part 2: {good2}")
 
 if __name__ == '__main__':
     main()
