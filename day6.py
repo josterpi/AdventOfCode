4f591adf2f2f569852bc4a12e96a5b3e3a4334c4
Jeremy Osterhouse <josterhouse@gmail.com>
2020-12-08 12:40:55 -0500

    Day 6, part 2

--- day6.py
+++ day6.py
@@ -4,6 +4,11 @@ def main():
         input = [x for x in f.read().split("\n\n")]
     total = sum(len(set(list(x.replace('\n', '')))) for x in input)
     print(f"Part 1: {total}")
+    total2 = 0
+    for group in input:
+        answers = [set(list(x)) for x in group.split()]
+        total2 += len(set.intersection(*answers))
+    print(f"Part 2: {total2}")
 
 
 if __name__ == '__main__':
