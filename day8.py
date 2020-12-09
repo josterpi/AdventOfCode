daadc2890a3a2d094fdee252dac3cc00cae372af
Jeremy Osterhouse <jeremy@onetencommunications.com>
2020-12-09 07:14:17 -0500

    Day 8, part 1

---
+++ day8.py
@@ -0,0 +1,47 @@
+
+NOP, ACC, JMP = ('nop', 'acc', 'jmp')
+
+test_input = """nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6"""
+
+def parse_input(input):
+    parsed = []
+    for line in input:
+        instr, arg = line.split()
+        number = int(arg[1:])
+        if arg[0] == '-':
+            number = -number
+        parsed.append((instr, number))
+    return parsed
+
+def main():
+    input = [line.strip() for line in open("day8.txt")]
+    # input = test_input.split('\n')
+    input = parse_input(input)
+    accumulator = 0
+    pointer = 0
+    run_count = [0] * len(input)
+    # import pdb; pdb.set_trace()
+    while True:
+        instr, arg = input[pointer]
+        run_count[pointer] += 1
+        if run_count[pointer] == 2:
+            break
+        if instr == NOP:
+            pointer += 1
+        if instr == ACC:
+            accumulator += arg
+            pointer += 1
+        if instr == JMP:
+            pointer += arg
+    print(f"Part 1: {accumulator}")
+
+if __name__ == '__main__':
+    main()
