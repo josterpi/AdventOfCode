71b1e9e12b43607fce01bbbd21b164b0effaa292
Jeremy Osterhouse <jeremy@onetencommunications.com>
2021-12-09 22:29:51 -0500

    Day 3, first pass at part 2
    This doesn't actually solve it. I'm committing to show the process of
    developing a solution.

--- day3.py
+++ day3.py
@@ -1,9 +1,9 @@
 
 def main():
     input = [line.strip() for line in open("input/day3.txt")]
-    num_digits = range(len(input[0]))
+    num_digits = len(input[0])
     input_cols = []
-    for i in num_digits:
+    for i in range(num_digits):
         input_cols.append([int(x[i]) for x in input])
     gamma = []
     epsilon = []
@@ -15,6 +15,18 @@ def main():
             gamma.append('0')
             epsilon.append('1')
     print("Part 1: %s" % (int(''.join(gamma), 2) * int(''.join(epsilon), 2)))
+
+    # Part 2
+    filtered_input = input.copy()
+    for i in range(num_digits):
+        digits = [int(x[i]) for x in filtered_input]
+        filter = '0'
+        if sum(digits) >= len(digits)/2:
+            filter = '1'
+        filtered_input = [x for x in filtered_input if x[i] == filter]
+        if len(filtered_input) == 1:
+            break
+    print(filtered_input)
 
 
 if __name__ == "__main__":
