ec9c5360fbefab5bc01da95e8c3ce634479a9b5d
Jeremy Osterhouse <jeremy@onetencommunications.com>
2021-12-09 21:08:25 -0500

    Day 1, Part 2

--- day1.py
+++ day1.py
@@ -7,6 +7,13 @@ def main():
             count += 1
     print(f"Part 1: {count}")
 
+    # Part 2
+    count2 = 0
+    for i in range(len(input) - 2):
+        if sum(input[i+1:i+4]) > sum(input[i:i+3]):
+            count2 += 1
+    print(f"Part 2: {count2}")
+
 
 if __name__ == '__main__':
     main()
