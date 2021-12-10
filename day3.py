71b4652f42daf6cdf3f14babb29dba101fdf8b00
Jeremy Osterhouse <jeremy@onetencommunications.com>
2021-12-09 22:46:25 -0500

    Day 3, Part 2

--- day3.py
+++ day3.py
@@ -17,16 +17,28 @@ def main():
     print("Part 1: %s" % (int(''.join(gamma), 2) * int(''.join(epsilon), 2)))
 
     # Part 2
-    filtered_input = input.copy()
-    for i in range(num_digits):
-        digits = [int(x[i]) for x in filtered_input]
-        filter = '0'
+    def filter_rating(input, filter_func):
+        filtered_input = input.copy()
+        for i in range(num_digits):
+            digits = [int(x[i]) for x in filtered_input]
+            filter = filter_func(digits)
+            filtered_input = [x for x in filtered_input if x[i] == filter]
+            if len(filtered_input) == 1:
+                return filtered_input[0]
+
+    def oxygen_filter(digits):
         if sum(digits) >= len(digits)/2:
-            filter = '1'
-        filtered_input = [x for x in filtered_input if x[i] == filter]
-        if len(filtered_input) == 1:
-            break
-    print(filtered_input)
+            return '1'
+        return '0'
+
+    def cotwo_filter(digits):
+        if sum(digits) >= len(digits)/2:
+            return '0'
+        return '1'
+
+    oxygen_rating = int(filter_rating(input, oxygen_filter), 2)
+    cotwo_rating = int(filter_rating(input, cotwo_filter), 2)
+    print(f"Part 2: {oxygen_rating*cotwo_rating}")
 
 
 if __name__ == "__main__":
