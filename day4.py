9fb54c75e3115dac98b9c98dc51f1f6f0fe15a49
Jeremy Osterhouse <jeremy@onetencommunications.com>
2021-12-26 15:15:36 -0500

    Day 4, part 1

---
+++ day4.py
@@ -0,0 +1,57 @@
+
+class Board:
+    def __init__(self, board):
+        self.board = board
+        self.marks = [[False for _ in range(5)] for _ in range(5)]
+        self.last_draw = None
+
+    def register_draw(self, draw):
+        self.last_draw = draw
+        for i, board_line in enumerate(self.board):
+            for j, spot in enumerate(board_line):
+                if spot == draw:
+                    self.marks[i][j] = True
+
+    def won(self):
+        for row in self.marks:
+            if all(row):
+                return True
+        for column in [[x[n] for x in self.marks] for n in range(5)]:  # transpose
+            if all(column):
+                return True
+        return False
+
+    def score(self):
+        "sum of all unmarked numbers * last number drawn"
+        unmarked = 0
+        for i, board_line in enumerate(self.board):
+            for j, x in enumerate(board_line):
+                if not self.marks[i][j]:
+                    unmarked += int(x)
+        return unmarked * int(self.last_draw)
+
+
+def main():
+    draws = []
+    boards = []
+    input_lines = [line.strip() for line in open("input/day4.txt")]
+    draws = input_lines[0].split(',')
+    board = []
+    for line in input_lines[2:]:
+        if line == '':
+            boards.append(board)
+            board = []
+        else:
+            board.append(line.split())
+
+    boards = [Board(b) for b in boards]
+    for draw in draws:
+        for b in boards:
+            b.register_draw(draw)
+            if b.won():
+                print(f"Part 1: {b.score()}")
+                return
+
+
+if __name__ == '__main__':
+    main()
